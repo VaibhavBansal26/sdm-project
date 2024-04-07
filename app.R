1e3352cff994130d8ddbb7ed78767865143090b7
Vaibhav Bansal <vaibhav.bansal945@gmail.com>
2024-04-07 13:42:43 -0400

    Basic R Setup

--- app.R
+++ app.R
@@ -1,53 +1,102 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
-
+# #
+# # This is a Shiny web application. You can run the application by clicking
+# # the 'Run App' button above.
+# #
+# # Find out more about building applications with Shiny here:
+# #
+# #    https://shiny.posit.co/
+# #
+# 
 library(shiny)
+library(shinydashboard)
+library(DT)
+library(DBI)
+library(RSQLite)
+library(ggplot2)
+library(shinythemes)
+library(shinyjs)
+library(shinycssloaders)
+library(lubridate)
+library(shinyFeedback)
+library(dplyr)
+library(dbplyr)
 
-# Define UI for application that draws a histogram
 
-ui <- fluidPage(
+# Connect to your SQLite database
+sqldb_connection <- dbConnect(RSQLite::SQLite(), 'data/sdm_netflix.sqlite')
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+# Define the UI
+ui <- dashboardPage(
+  skin="green",
+  dashboardHeader(title = "SDM Project"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Home", tabName = "home", icon = icon("home")),
+      menuItem("Data View", tabName = "dataview", icon = icon("table")),
+      menuItem("Analysis", tabName = "charts", icon = icon("bar-chart")),
+      menuItem("Charts", tabName = "charts", icon = icon("bar-chart")),
+      menuItem("About", tabName = "about", icon = icon("info-circle"))
     )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "home",
+              h2("Welcome to the SDM Project"),
+              h3("Team Members"),
+              h4("1. Vaibhav Bansal - Employee"),
+              h4("2. Devendra Rana - CEO"),
+              h4("3. Mounika - CMO"),
+              h4("4. Atul Sharma - CTO")
+      ),
+      tabItem(tabName = "dataview",
+              h2("Data View"),
+              downloadButton("downloadData", "Download Data"),
+              br(),
+              DT::dataTableOutput("dataView")
+      ),
+      tabItem(tabName = "charts",
+              h2("Charts"),
+              sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30),
+              plotOutput("histPlot")
+      ),
+      tabItem(tabName = "about",
+              h2("About the Team"),
+              p("Vaibhav"),
+              p("Devendra"),
+              p("Mounika"),
+              p("Atul")
+      )
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+# Server logic
+server <- function(input, output) { 
+  # Data View
+  output$dataView <- DT::renderDataTable({
+    query <- sprintf("SELECT * FROM netflix")
+    data <- dbGetQuery(sqldb_connection, query)
+    DT::datatable(data)
+  })
+  
+  # Charts (Histogram)
+  output$histPlot <- renderPlot({
+    data <- rnorm(100)
+    bins <- seq(min(data), max(data), length.out = input$bins + 1)
+    hist(data, breaks = bins, col = 'lightblue', border = 'white')
+  })
+  
+  output$downloadData <- downloadHandler(
+        filename = function() {
+          paste("data-", Sys.Date(), ".csv", sep = "")
+        },
+        content = function(file) {
+          query <- "SELECT * FROM netflix"
+          data <- dbGetQuery(sqldb_connection, query)
+          write.csv(data, file, row.names = FALSE)
+        }
+    )
 }
 
 # Run the application
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
--- rsconnect/deployapp.R
+++ rsconnect/deployapp.R
@@ -1,9 +1,8 @@
-rsconnect::setAccountInfo(name='vaibhavbansal',
-                          token='FCABEBADC2E63A5CCA07719D1EFDD26A',
-                          secret='wE61BCvUml1nMJvBCkAbEa2fnc0xgA0IBI85dV9v')
-
+rsconnect::setAccountInfo(name='vaibhavbansal',token='FCABEBADC2E63A5CCA07719D1EFDD26A',secret='wE61BCvUml1nMJvBCkAbEa2fnc0xgA0IBI85dV9v')
 library(rsconnect)
-
 rsconnect::deployApp('/Users/vaibhavbansal/ubprojects/sdm/Sdmproject')
+
+
+
 
 
