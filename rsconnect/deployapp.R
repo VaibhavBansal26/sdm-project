98632e09e19b773e67a368a73b86d4cfb29afa7d
Vaibhav Bansal <vaibhav.bansal945@gmail.com>
2024-04-06 22:20:33 -0400

    SDM Project Initialisation

---
+++ rsconnect/deployapp.R
@@ -0,0 +1,9 @@
+rsconnect::setAccountInfo(name='vaibhavbansal',
+                          token='FCABEBADC2E63A5CCA07719D1EFDD26A',
+                          secret='wE61BCvUml1nMJvBCkAbEa2fnc0xgA0IBI85dV9v')
+
+library(rsconnect)
+
+rsconnect::deployApp('/Users/vaibhavbansal/ubprojects/sdm/Sdmproject')
+
+
